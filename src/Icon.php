7601c48474cd9563eed7c1afb45a2de600c926da
Bagus A Priambada <iam.adty@gmail.com>
2020-11-30 07:52:03 +0700

    icon html

---
+++ src/Icon.php
@@ -0,0 +1,24 @@
+<?php namespace PWC\Component\Html;
+
+use PWC\BuilderTrait;
+use PWC\Component;
+
+class Icon extends Component
+{
+    protected $_ID = 'pwc-html-icon';
+    protected $type = 'fa';
+
+    public function render(): string
+    {
+        $name = parent::render();
+        return (string) Italic::build()->class("{$this->type} {$this->type}-{$name}");
+    }
+
+    public function type($type = 'fa')
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    use BuilderTrait;
+}
